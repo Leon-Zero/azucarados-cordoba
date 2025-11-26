7ff2275af70a1508bf4968c290f2a30de4d60c93
Leonardo Ismael HIDALGO <leon.cba09@gmail.com>
2025-11-26 12:26:05 -0300

    form contacto conectado a formsubmit

--- src/app/shared/forms/contact/contact.form.ts
+++ src/app/shared/forms/contact/contact.form.ts
@@ -10,4 +10,15 @@ import { SizeButton } from '../../../data/interfaces/size-button.interface';
 })
 export class ContactForm {
   _size = signal<SizeButton>('large');
+  mail: string = 'leon.cba09@gmail.com';
+  asunto: string = 'WEB Azucarados Cordoba - form. contacto';
+  web: string = 'http://localhost:4200';
+  autoResponse: string = `Muchas gracias por escribir, su consulta será analizada y me estaré comunicando a la brevedad.
+    Atte.: Azucarados Córdoba`;
+  return: string = this.web;
+  contactMethod: 'telefono' | 'mail' | null = null;
+
+  onContactMethodChange(value: 'telefono' | 'mail') {
+    this.contactMethod = value;
+  }
 }
