159dca74c7db4c83670f471b01ed61648407de12
Leonardo Ismael HIDALGO <leon.cba09@gmail.com>
2025-11-27 00:31:40 -0300

    page admin, opciones de edicion

---
+++ src/app/routes/pages/crud-admin/crud-admin.page.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { Btn } from '../../../shared/ui/btn/btn';
+
+@Component({
+  selector: 'app-crud-admin',
+  imports: [Btn],
+  templateUrl: './crud-admin.page.html',
+  styleUrl: './crud-admin.page.css',
+})
+export class CrudAdminPage {}
